baf19e53c09f4e58e78fc6dad75614c41c056cfa
James Pier <jpier2012@gmail.com>
2019-05-23 20:00:50 +0000

    Automatically backed up by Learn

--- db/migrate/006_add_attributes_to_shows.rb
+++ db/migrate/006_add_attributes_to_shows.rb
@@ -1,4 +1,4 @@
-class AddAttributesToShow < ActiveRecord::Migration[5.1]
+class AddAttributesToShow < ActiveRecord::Migration[5.2]
   def change
     add_column :shows, :day, :string
     add_column :shows, :season, :string
